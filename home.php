feb6d5ad7dad5f5d272ebac577c7b81ecba21406
Rand Alzaben <85239594+rand-alzaben@users.noreply.github.com>
2022-04-18 18:49:41 +0200

    my first commit
    first form 
    first homepage

---
+++ home.php
@@ -0,0 +1,11 @@
+<?php
+echo "your first name is " . $_POST["first_name"];
+echo "<br>";
+echo "your last name is " . $_POST["last_name"];
+echo "<br>";
+echo "your  age is " . $_POST["age"]; 
+echo "<br>";
+echo "your number is " . $_POST["number"];
+echo "<br>"; 
+echo "your birthday date is " . $_POST["birth_date"];
+?>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+</head>
+
+<body>
+    <form action="home.php" method="POST">
+    firstname <input type="text" name="first_name"><br>
+    lastname <input type="text" name="last_name"> <br>
+    age <input type="number" name="age"> <br>
+    number <input type="text" name="number"><br>
+    birthday date <input type="date" name="birth_date"><br>
+        <input type="submit" name="submit">
+    </form>
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
